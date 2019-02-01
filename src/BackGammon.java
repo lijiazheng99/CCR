4fc218acca3d30a57411dcdc8fa8fd32fc14d422
jiazheng <jiazheng.li@ucdconnect.ie>
2019-02-01 10:28:36 +0000

    Commit by Jiazheng 1. Delete the space in the name of jpeg 2. Wrote main function

--- src/BackGammon.java
+++ src/BackGammon.java
@@ -1,3 +1,52 @@
-public class BackGammon {
+
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class BackGammon extends Application
+{
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("HTML");
+        stage.setWidth(500);
+        stage.setHeight(500);
+        Scene scene = new Scene(new Group());
+        VBox root = new VBox();
+
+        final ImageView selectedImage = new ImageView();
+        Image image1 = new Image("file:///C:/Users/user/Desktop/x.jpg");
+
+        selectedImage.setImage(image1);
+
+        root.getChildren().addAll(selectedImage);
+
+        scene.setRoot(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
---
+++ src/MainInterface.java
@@ -0,0 +1,11 @@
+
+/* Create by Jiazheng Li
+on 1st Feb 2019 */
+
+
+
+
+
+
+public class MainInterface {
+}
