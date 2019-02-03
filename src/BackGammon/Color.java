0512005ac9f40f13d12da487c6089ebdcac59278
jiazheng <jiazheng.li@ucdconnect.ie>
2019-02-03 02:39:56 +0000

    Jiazheng Li update color enum type

--- src/BackGammon/Color.java
+++ src/BackGammon/Color.java
@@ -1,5 +1,12 @@
 package BackGammon;
 //created by Jiwei Zhang, 1/1/2019
 public enum Color {
-    Red, White, Empty;
+    Red(2), White(1), Empty(0);
+
+    final int moveDir;
+
+    Color(int moveDir)
+    {
+        this.moveDir = moveDir;
+    }
 }
