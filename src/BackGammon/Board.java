a8f3110421c73f6fe98bf77a2f84c996553c46a6
NoobVic <44397320+602730675@users.noreply.github.com>
2019-03-24 19:29:41 +0000

    when Hitnum = 1  use the first number to do re-enter  second for normal move

--- src/BackGammon/Board.java
+++ src/BackGammon/Board.java
@@ -106,6 +106,24 @@ public class Board {
         return false;
     }
 
+    public void reEnter(Checker_Color c, int points)
+    {
+        if(c == Checker_Color.RED)
+            bars[0+points].moveIn(c);
+        else if(c == Checker_Color.WHITE)
+            bars[25-points].moveIn(c);
+        else;
+    }
+
+    public void reEnterUndo(Checker_Color c, int points)
+    {
+        if(c == Checker_Color.RED)
+            bars[0+points].moveOut();
+        else if(c == Checker_Color.WHITE)
+            bars[25-points].moveOut();
+        else;
+    }
+
     public boolean checkMove(Checker_Color c, int start, int points)
     {
         if(points == 0)
@@ -127,6 +145,15 @@ public class Board {
         else return false;
     }
 
+    public boolean checkHit(Checker_Color c, int index)
+    {
+        if(c == Checker_Color.RED)
+            return bars[25-index].checkKick(c);
+        else if(c == Checker_Color.WHITE)
+            return bars[index].checkKick(c);
+        return false;
+    }
+
     public boolean checkRepeat(MoveRecord mr) {
         for (int i = 0; i < moveList.length; i++)
         {if (mr.equalsTo(moveList[i]))
@@ -139,6 +166,7 @@ public class Board {
         moveList = new MoveRecord[50];
         boolean reEnter;
         boolean doubles;
+        boolean exist;
         points1 = p1;
         points2 = p2;
 
@@ -161,14 +189,42 @@ public class Board {
         if(doubles)//double situation
         {
             int number = points1;
+
         }else {//not a double situation
             if(reEnter)
             {
                 if(currHit == 1)
                 {
-                    if(checkReEnter(c,points1))
+                    if(checkReEnter(c,points1))//points1 can be used to re-enter
                     {
                         //calculate points2 normal move
+                        //assume the Re-enter
+                        reEnter(c,points1);
+                        exist = false;
+                        for(int i = 24; i >= 1; i--)
+                        {
+                            if(checkMove(c,i,points2))
+                            {
+                                exist = true;
+                                curr.setMoveOne(25,25-points1, checkHit(c,25-points1));
+                                curr.setMoveTwo(i, i-points2,checkHit(c,i-points2));
+                                if(!checkRepeat(curr)){
+                                    moveList[count++] = curr;
+                                    curr = new MoveRecord();
+                                }
+                            }
+                        }
+                        if(!exist)//second move doesn't exist
+                        {
+                            curr.setMoveOne(25,25-points1, checkHit(c,25-points1));
+                            curr.setMoveTwo(-1,-1,false);
+                            if(!checkRepeat(curr)){
+                                moveList[count++] = curr;
+                                curr = new MoveRecord();
+                            }
+                        }
+                        reEnterUndo(c,points1);
+                        //Undo the assume
                     }
 
                     if(checkReEnter(c,points2))
