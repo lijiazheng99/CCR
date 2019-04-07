4a6dcfbb9bd2dc9807d8f032abd8d2d02c94ba83
NoobVic <44397320+602730675@users.noreply.github.com>
2019-04-07 13:33:58 +0100

    Double Cube class update

---
+++ src/BackGammon/DoubleCude.java
@@ -0,0 +1,42 @@
+package BackGammon;
+
+public class DoubleCude
+{
+	private Player currentPlayer;
+	private int currentPoints;
+	private final int MAX = 64;
+	private final int RESET = 1;
+	
+	public DoubleCude()
+	{
+		reset();
+	}
+	
+	public Player getCurrentPlayer()
+	{
+		return currentPlayer;
+	}
+	
+	public int getCurrentPoints()
+	{
+		return currentPoints;
+	}
+	
+	public boolean doubleThePoints(Player p)
+	{
+		if(p != currentPlayer && currentPoints != MAX)
+		{
+			currentPlayer = p;
+			currentPoints *= 2;
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public void reset()
+	{
+		currentPlayer = null;
+		currentPoints = RESET;
+	}
+}
