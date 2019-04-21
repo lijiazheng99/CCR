f2df9e749929c3b92cd8dcdf7f5d57a7b66ab4a9
NoobVic <44397320+602730675@users.noreply.github.com>
2019-04-21 16:14:47 +0100

    Class" CCR_Bot" structures finished

--- src/Sprint5/CCR_Bot.java
+++ src/Sprint5/CCR_Bot.java
@@ -24,6 +24,13 @@ public class CCR_Bot implements BotAPI{
         // Add your code here
         int playNumber = 0;
 
+        int boardScores[] = new int[possiblePlays.number()];
+        for(int i = 0; i < possiblePlays.number(); i++)
+        {
+            boardScores[i] = getBoardScore(possiblePlays,i);
+            if(boardScores[playNumber] < boardScores[i])
+                playNumber = i;
+        }
         return Integer.toString(playNumber);
     }
 
@@ -32,4 +39,37 @@ public class CCR_Bot implements BotAPI{
         return "n";
     }
 
+    public int getBoardScore(Plays possiblePlays, int index)
+    {
+        int[][] boardCopy = board.get();
+        Play targetPlay = possiblePlays.get(index);
+        move(boardCopy,targetPlay);
+
+        int boardScore = 0;
+        //boardScore + features calculators;
+        //TODO
+        return boardScore;
+    }
+
+    private void move(int[][] boardCopy, Play targetPlay)
+    {
+        for (Move move : targetPlay) {
+            move(boardCopy,move);
+        }
+    }
+
+    private void move(int[][] boardCopy, Move targetMove)
+    {
+        boardCopy[me.getId()][targetMove.getFromPip()]--;
+        boardCopy[me.getId()][targetMove.getToPip()]++;
+        if (targetMove.getToPip()<Board.BAR && targetMove.getToPip()>Board.BEAR_OFF &&
+                boardCopy[opponent.getId()][calculateOpposingPip(targetMove.getToPip())] == 1) {
+            boardCopy[opponent.getId()][calculateOpposingPip(targetMove.getToPip())]--;
+            boardCopy[opponent.getId()][Board.BAR]++;
+        }
+    }
+
+    private int calculateOpposingPip(int pip) {
+        return Board.NUM_PIPS-pip+1;
+    }
 }
