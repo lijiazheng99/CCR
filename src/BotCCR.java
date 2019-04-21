4bb1b5658f8953e4fb48e3dbc05f299380b29aaf
17200334 <jiwei.zhang@ucdconnect.ie>
2019-04-21 22:57:56 +0100

    add 2 features

--- src/BotCCR.java
+++ src/BotCCR.java
@@ -5,6 +5,13 @@ public class BotCCR implements BotAPI{
     private MatchAPI match;
     private InfoPanelAPI info;
 
+    //SLOPE HERE:
+    //TODO
+    private final int BASIC_SLOPE = 1;
+    private final int KICK_SLOPE = 2;
+
+
+
     BotCCR (PlayerAPI me, PlayerAPI opponent, BoardAPI board, CubeAPI cube, MatchAPI match, InfoPanelAPI info) {
         this.me = me;
         this.opponent = opponent;
@@ -20,20 +27,20 @@ public class BotCCR implements BotAPI{
 
     public String getCommand(Plays possiblePlays) {
         // Add your code here
-        int playNumber = 0;
-
+        int bestChoice = 0;
         int boardScores[] = new int[possiblePlays.number()];
+
         for(int i = 0; i < possiblePlays.number(); i++)
         {
             boardScores[i] = getBoardScore(possiblePlays,i);
-            if(boardScores[playNumber] < boardScores[i])
-                playNumber = i;
+            if(boardScores[bestChoice] < boardScores[i])
+                bestChoice = i;
         }
-        return Integer.toString(playNumber+1);
+
+        return Integer.toString(bestChoice+1);
     }
 
     public String getDoubleDecision() {
-        //TODO
         int[][] boardCopy = board.get();
         int myPip = 0;
         int oppoPip = 0;
@@ -42,11 +49,11 @@ public class BotCCR implements BotAPI{
             myPip += boardCopy[me.getId()][i];
             oppoPip += boardCopy[opponent.getId()][i];
         }
-        if((myPip >= 100) && (myPip + getBoardScore(boardCopy) <= 1.2*(oppoPip + getOppositeBoardScore(boardCopy))))
+        if((myPip >= 100) && (myPip + getBoardScore(me,boardCopy) <= 1.2*(oppoPip + getBoardScore(opponent,boardCopy))))
             return "y";//we may win then just double it
-        else if((myPip >= 50 && myPip <= 100) && (myPip + getBoardScore(boardCopy) <= 1.1*(oppoPip + getOppositeBoardScore(boardCopy))))
+        else if((myPip >= 50 && myPip <= 100) && (myPip + getBoardScore(me,boardCopy) <= 1.1*(oppoPip + getBoardScore(opponent,boardCopy))))
             return "y";
-        else if((myPip >= 10 && myPip <= 50) && (myPip + getBoardScore(boardCopy) <= (oppoPip + getOppositeBoardScore(boardCopy))))
+        else if((myPip >= 10 && myPip <= 50) && (myPip + getBoardScore(me,boardCopy) <= (oppoPip + getBoardScore(opponent,boardCopy))))
             return "y";
         else if(me.getScore() < opponent.getScore() && (opponent.getScore() + cube.getValue()) > match.getLength())
             return "y";//if I give up, I will lose the match
@@ -59,26 +66,18 @@ public class BotCCR implements BotAPI{
         int[][] boardCopy = board.get();
         Play targetPlay = possiblePlays.get(index);
         move(boardCopy,targetPlay);
-
-        int boardScore = getBoardScore(boardCopy);
-
+        int boardScore = getBoardScore(me,boardCopy);
         return boardScore;
     }
 
-    public int getBoardScore(int[][] boardCopy)
+    public int getBoardScore(PlayerAPI currentPlayer, int[][] boardCopy)
     {
         int boardScore = 0;
         //boardScore + features calculators;
-        //FORMULA: boardScore += XXX_SLOPE*numOfXXX(boardCopy);
-        //TODO
-        return boardScore;
-    }
-
-    public int getOppositeBoardScore(int[][] boardCopy)
-    {
-        int boardScore = 0;
-        //boardScore + features calculators;
-        //TODO
+        //FORMULA: boardScore += XXX_SLOPE*Finder(player,boardCopy);
+        //TODO:
+        boardScore += BASIC_SLOPE * basicBoardScore(currentPlayer,boardCopy);
+        boardScore += KICK_SLOPE * kickOffBoardScore(currentPlayer,boardCopy);
         return boardScore;
     }
 
@@ -102,5 +101,39 @@ public class BotCCR implements BotAPI{
 
     private int calculateOpposingPip(int pip) {
         return Board.NUM_PIPS-pip+1;
+    }
+
+    private int basicBoardScore(PlayerAPI player, int[][] boardCopy)
+    {
+        int counter = 0;
+
+        for(int i = 0; i <= Board.BAR; i++)
+        {
+            if(i == 0)
+            {
+                counter += 5*boardCopy[player.getId()][i];//more bear off get higher score
+            }
+            else if(i == 25)
+            {
+                counter += (-5)*boardCopy[player.getId()][i];//more bar get lower score
+            }
+            else//1-24
+            {
+                if(boardCopy[player.getId()][i] == 1)
+                    counter += (-2);//single checker left
+                else if(boardCopy[player.getId()][i] > 1)
+                    counter += 2;//checkers group
+            }
+        }
+
+        return counter;
+    }
+
+    private int kickOffBoardScore(PlayerAPI player, int[][] boardCopy)
+    {
+        if(player == me)
+            return boardCopy[opponent.getId()][25];
+        else
+            return boardCopy[me.getId()][25];
     }
 }
