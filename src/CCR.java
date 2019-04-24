83b966d13e19e61938bce1c9e38d37fab922e338
Lorand <lorand.neculae@ucdconnect.ie>
2019-04-24 03:05:41 +0100

    UPDATE parameter

--- src/CCR.java
+++ src/CCR.java
@@ -51,6 +51,7 @@ public class CCR implements BotAPI{
             {
                 boardScores[i] = getBoardScore(possiblePlays,i);
                 if(boardScores[bestChoice] < boardScores[i])
+
                     bestChoice = i;
             }
 
