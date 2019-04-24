91fd5ef68b98db9bac3bd32d787714991dc88eaa
Lorand <lorand.neculae@ucdconnect.ie>
2019-04-24 03:07:49 +0100

    UPDATE parameter

--- src/BotTest.java
+++ src/BotTest.java
@@ -7,9 +7,9 @@ public class BotTest implements BotAPI{
 
     //SLOPE HERE:
     private final int BASIC_SLOPE = 3;
-    private final int PIP_COUNT_SLOPE = 3;
+    private final int PIP_COUNT_SLOPE = 2;
     private final int BAR_SLOPE = -3;
-    private final int BEAR_OFF_SLOPE = 3;
+    private final int BEAR_OFF_SLOPE = 1;
     private final int SINGLE_SLOPE = -3;
     private final int KICK_SLOPE = 3;
     private final int PRIME_SLOPE= 3;
